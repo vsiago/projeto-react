ea089a15f9425f10fe1910ba30ad16b96fdcf9ea
vsiago <101620032+vsiago@users.noreply.github.com>
2022-12-01 14:14:05 +0000

    atualizacao de estilo

--- src/components/Frase.js
+++ src/components/Frase.js
@@ -1,6 +1,10 @@
+import styles from './Frase.module.css'
+
 function Frase({ nome }) {
   return (
-    <p>Meu nome é: {nome}</p>
+    <div className={styles.fraseContainer}>
+      <p className={styles.fraseContent}>Meu nome é: {nome}</p>
+    </div>
   )
 }
 
